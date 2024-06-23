8c995cc8764bec5c7e35ad7c50f1b75f48846804
RFigueroa <48028483+FRobertoFigueroaC@users.noreply.github.com>
2024-06-23 16:00:47 -0600

    Broadcast and frontend

---
+++ README.md
@@ -0,0 +1,6 @@
+## Instructions
+
+- Install packages `npm install`
+- Run `npm run dev`
+- Go to frontend folder and run - `npx http-server -o`
+- Websocket server will run in port http://127.0.0.1:3000 and frontend client will run in http://127.0.0.1:8080
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,38 @@
 
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer( { port: 3000 } );
 
 wss.on( 'connection', function connection( ws ) {
+
+
   console.log('Client connected');
 
   ws.on( 'error', console.error );
 
   ws.on( 'message', function message( data ) {
-    console.log( 'received: %s', data );
+    
+
+    const payload = {
+      type: 'custom-message',
+      payload: data.toString()
+    }
+
+    // ws.send( JSON.stringify( payload ) );
+    wss.clients.forEach(client => {
+      // Envia a todos 
+      // if (client.readyState === WebSocket.OPEN) {
+      // Envia a todos excepto en que envia
+      if ( client !== ws && client.readyState === WebSocket.OPEN ) {
+        client.send( JSON.stringify( payload ), { binary: false } );
+      }
+    });
+
+  });
+
+  ws.on( 'close', () => {
+    console.log('Client disconnected');
   } );
 
-  setInterval(() => {
-    ws.send( 'Hola desde el servidor' );
-  }, 2000);
 
-  
 } );
